fbf206afc68423cf10671d75b34eca6f5e8e2468
yykdev <assavictory@gmail.com>
2018-01-25 15:11:06 +0900

    기본 로그인, 프로필 출력 구성

--- django_app/accounts/urls.py
+++ django_app/accounts/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
+
+from .views import profile
 
 app_name = 'accounts'
 
 urlpatterns = [
-    
+    url(r'^login/$', login, name='login', kwargs={
+        'template_name': 'accounts/login.html',
+    }),
+    url(r'profile/$', profile, name='profile'),
 ]

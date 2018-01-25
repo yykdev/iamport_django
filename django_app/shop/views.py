b7beb775257c5710b275d1509c651467120a9011
yykdev <assavictory@gmail.com>
2018-01-25 17:49:03 +0900

    검색 기능 추가

--- django_app/shop/views.py
+++ django_app/shop/views.py
@@ -3,4 +3,21 @@ from django.views.generic import ListView
 
 from shop.models import Item
 
-index = ListView.as_view(model=Item, queryset=Item.objects.filter(is_public=True))
+
+class ItemListView(ListView):
+    model = Item
+    queryset = Item.objects.filter(is_public=True)
+
+    def get_queryset(self):
+        self.q = self.request.GET.get('q','')
+        qs = super().get_queryset()
+        if self.q:
+            qs = qs.filter(name__icontains=self.q)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['q'] = self.q
+        return context
+
+index = ItemListView.as_view()

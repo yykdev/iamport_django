e593c6a5853b400a524a155f282111bed6df784b
yykdev <assavictory@gmail.com>
2018-01-25 15:26:40 +0900

    네비게이션 구성 셋팅

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.shortcuts import redirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
+    url(r'^$', lambda request: redirect('shop:index'), name='root'),
 ]

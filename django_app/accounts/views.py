176757420dc8748451c6c64405affda7285525f6
yykdev <assavictory@gmail.com>
2018-01-26 10:11:31 +0900

    프로필 화면 주문내역 확인

--- django_app/accounts/views.py
+++ django_app/accounts/views.py
@@ -2,8 +2,15 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from shop.models import Order
 
 
 @login_required
 def profile(request):
-    return render(request, 'accounts/profile.html')
+    order_list = Order.objects.filter(user=request.user)
+    # order_list = request.user.order_set.all()
+
+    context = {
+        'order_list': order_list,
+    }
+    return render(request, 'accounts/profile.html', context=context)

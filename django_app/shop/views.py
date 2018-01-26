d37726da10ba8f3f2c732317ae5bc131c38dd9e7
yykdev <assavictory@gmail.com>
2018-01-26 10:55:58 +0900

    아임포트 기본 셋팅

--- django_app/shop/views.py
+++ django_app/shop/views.py
@@ -41,4 +41,5 @@ def order_new(request, item_id):
         form = OrderForm(initial=initial)
     return render(request, 'shop/order_form.html', {
         'form':form,
+        'iamport_shop_id': 'iamport',  # FIXME : 가맹점 식별 코드
     })

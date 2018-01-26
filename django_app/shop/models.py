be1c85fbe0452db8a4ae8474dfb90f3a3f6d86a9
yykdev <assavictory@gmail.com>
2018-01-26 09:38:02 +0900

    shop Order 모델 생성 및 마이그레이션

--- django_app/shop/models.py
+++ django_app/shop/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 
@@ -11,3 +12,10 @@ class Item(models.Model):
     is_public = models.BooleanField(default=False, db_index=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+class Order(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    item = models.ForeignKey(Item)
+    name = models.CharField(max_length=100, verbose_name='상품명')
+    amount = models.PositiveIntegerField(verbose_name='결제금액')
